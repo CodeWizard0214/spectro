0ff90e8db02ca9692bb6fd04a66e2db834944968
codewizard0214 <codewizard0214@gmail.com>
2024-02-22 12:32:38 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+import Waveform from "./Waveform";
+import PlayList from "./PlayList";
+
+// const url = "https://www.mfiles.co.uk/mp3-downloads/gs-cd-track2.mp3";
+
+const tracks = [
+  {
+    id: 0,
+    title: "1.mp3",
+    url: "https://animal-delta.vercel.app/1.mp3",
+  },
+  {
+    id: 1,
+    title: "60429.mp3",
+    url: "https://animal-delta.vercel.app/1.mp3",
+  },
+];
+
+export default function App() {
+  const [selectedTrack, setSelectedTrack] = useState(tracks[0]);
+
+  return (
+    <div className="App">
+      <Waveform url={selectedTrack.url} />
+      <PlayList
+        tracks={tracks}
+        selectedTrack={selectedTrack}
+        setSelectedTrack={setSelectedTrack}
+      />
+      <br />
+    </div>
+  );
+}
